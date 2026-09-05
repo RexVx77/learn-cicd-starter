849a11ce81d59acd409eef5fb8b62ef075719311
RexVx77 <rexianasylum@gmail.com>
2026-09-05 20:40:23 +0530

    fix gosec issues

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -94,10 +96,16 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+		Addr:              ":" + port,
+		Handler:           router,
+		ReadHeaderTimeout: 5 * time.Second,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
+	portNum, err := strconv.Atoi(port)
+	if err != nil {
+		log.Fatalf("invalid PORT value")
+	}
+	log.Printf("Serving on port: %d\n", portNum)
+
 	log.Fatal(srv.ListenAndServe())
 }
